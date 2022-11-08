9bd2790d5e0dc66b26baf5d89820a1aa16e768af
yhwjd <yhwjd@naver.com>
2022-11-08 11:21:55 +0900

    memberService, postService Test

--- src/test/java/toyproject/board/service/MemberServiceTest.java
+++ src/test/java/toyproject/board/service/MemberServiceTest.java
@@ -1,9 +1,7 @@
 package toyproject.board.service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import toyproject.board.domain.Member;
@@ -13,13 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest  // @SpringBootTest 를 해야하는데 @SpringBootApplication 이라고 했다. // DB 실행하고 테스트하기
 @Transactional
 class MemberServiceTest {
 
     @Autowired
-    MemberService memberService;
+    private MemberService memberService;
 
     @Test
     public void 회원가입() {
@@ -33,9 +32,68 @@ class MemberServiceTest {
 
         Member member = new Member("userA", "1234", posts);
         //when
-        Member findMember = memberService.signup(member);
+        Member savedMember = memberService.signup(member);
         //then
-        assertThat(member).isEqualTo(findMember);
+        assertThat(member).isEqualTo(savedMember);
         assertThat(member.getPosts().size()).isEqualTo(2);
     }
+
+    @Test
+    public void 중복회원검증() {
+        //given
+        Post post1 = new Post();
+        Post post2 = new Post();
+
+        List<Post> posts1 = new ArrayList<>();
+        List<Post> posts2 = new ArrayList<>();
+        posts1.add(post1);
+        posts2.add(post2);
+
+        Member memberA = new Member("userA", "1234", posts1);
+        Member memberB = new Member("userA", "123456", posts2);
+        //when
+        memberService.signup(memberA);
+        //then
+        assertThrows(IllegalStateException.class,
+                () -> memberService.signup(memberB));
+    }
+
+    @Test
+    public void 이름으로_회원찾기() {
+        //given
+        Post post1 = new Post();
+        List<Post> posts1 = new ArrayList<>();
+        posts1.add(post1);
+
+        Member member = new Member("userA", "1234", posts1);
+        //when
+        memberService.signup(member);
+        Member findMemberByUsername = memberService.findByUsername("userA");
+        //then
+        assertThat(member).isEqualTo(findMemberByUsername);
+        assertThat(member.getId()).isEqualTo(findMemberByUsername.getId());
+        assertThat(member.getUsername()).isEqualTo(findMemberByUsername.getUsername());
+        assertThat(member.getPassword()).isEqualTo(findMemberByUsername.getPassword());
+        assertThat(member.getPosts()).isEqualTo(findMemberByUsername.getPosts());
+    }
+
+    @Test
+    public void 아이디로_회원찾기() {
+        //given
+        Post post1 = new Post();
+        List<Post> posts1 = new ArrayList<>();
+        posts1.add(post1);
+
+        Member member = new Member("userA", "1234", posts1);
+        //when
+        memberService.signup(member);
+        Member findMemberById = memberService.findById(member.getId());
+        //then
+        assertThat(member).isEqualTo(findMemberById);
+        assertThat(member.getId()).isEqualTo(findMemberById.getId());
+        assertThat(member.getUsername()).isEqualTo(findMemberById.getUsername());
+        assertThat(member.getPassword()).isEqualTo(findMemberById.getPassword());
+        assertThat(member.getPosts()).isEqualTo(findMemberById.getPosts());
+    }
+
 }
---
+++ src/test/java/toyproject/board/service/PostServiceTest.java
@@ -0,0 +1,59 @@
+package toyproject.board.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import toyproject.board.domain.Member;
+import toyproject.board.domain.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class PostServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private PostService postService;
+
+    @Test
+    public void 게시글_등록() {
+        //given
+        Post post = new Post("hello", "helloWorld");
+
+        List<Post> posts = new ArrayList<>();
+        posts.add(post);
+
+        Member member = new Member("userA", "1234", posts);
+        //when
+        memberService.signup(member);
+        post.setMember(member);
+        Post savedPost = postService.save(post);
+        //then
+        assertThat(post).isEqualTo(savedPost);
+        assertThat(post.getMember()).isEqualTo(savedPost.getMember());
+        assertThat(savedPost.getTitle()).isEqualTo("hello");
+        assertThat(savedPost.getBody()).isEqualTo("helloWorld");
+    }
+
+    @Test
+    public void 모든게시글_찾기() {
+        //given
+        Post post1 = new Post("hello1", "helloWorld1");
+        Post post2 = new Post("hello2", "helloWorld2");
+        Post post3 = new Post("hello3", "helloWorld3");
+
+        //when
+        List<Post> posts = new ArrayList<>();
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+        //then
+        assertThat(posts.size()).isEqualTo(3);
+    }
+}
